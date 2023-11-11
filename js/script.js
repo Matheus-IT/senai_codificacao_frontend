1d95a2e6f4b1125ab98a6de8b874474a42d83a13
Matheus-IT <matheuscosta.tech@gmail.com>
2023-11-11 16:25:59 -0300

    Back to top logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 //efeito do botão voltar ao Topo
+$('#back-to-top').on('click', goToTop);
 
+function goToTop() {
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: 'smooth',
+	});
+}
 //Validação de Login
 
 //Ativar alert no botão cadastrar

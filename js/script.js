16d84b1308455eed57c17885f20e6ef923a297c5
Matheus-IT <matheuscosta.tech@gmail.com>
2023-11-11 16:47:50 -0300

    Login form basic validation

--- js/script.js
+++ js/script.js
@@ -8,6 +8,26 @@ function goToTop() {
 		behavior: 'smooth',
 	});
 }
-//Validação de Login
 
+//Validação de Login
+$('#login-form').on('submit', handleLogin);
+const users = [
+	{username: 'matheus', password: 'password123'},
+	{username: 'admin', password: '123'},
+];
+
+function handleLogin(e) {
+	e.preventDefault();
+	
+	const username = $('#login-form [name=username]').val();
+	const password = $('#login-form [name=password]').val();
+
+	const userFound = users.find(u => u.username === username && u.password === password);
+
+	if (userFound) {
+		window.location = 'index.html';
+	} else {
+		alert('Accesso negado. Dados incorretos');
+	}
+}
 //Ativar alert no botão cadastrar
